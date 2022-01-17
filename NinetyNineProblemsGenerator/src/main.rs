445481ec8742c511e8312aee7aa150180475bbfb
makeit-mike <mike.isaac.jones@gmail.com>
2022-01-17 17:13:49 -0600

    adding json serde library. testing

--- NinetyNineProblemsGenerator/src/main.rs
+++ NinetyNineProblemsGenerator/src/main.rs
@@ -1,3 +1,22 @@
+extern crate serde_json;
+
+use serde_json::Value as JsonValue;
+
 fn main() {
-    println!("Hello, world!");
+    let json_str = r#"
+        {
+            "name":"michael",
+            "age": 24,
+            "is_male": true
+        }
+    "#;
+
+    let result = serde_json::from_str(json_str);
+
+    if result.is_ok() {
+        let parsed: serde_json::Value = result.unwrap();
+        println!("Name: {}", parsed["name"].as_str().unwrap());
+    } else {
+        println!("Failed to parse JSON.");
+    }
 }
